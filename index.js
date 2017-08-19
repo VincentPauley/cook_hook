96eb7ce0b908cdf8a23fb492ffbf8504fed67401
Vincent Pauley <vinniepauley@gmail.com>
2017-08-19 15:04:17 -0500

    initial page

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require( 'express' ),
     app = express();
 
+app.use( express.static( 'public' ) );
+
 app.get('/', function( req, res ) {
     res.send( 'Welcome to Cook-Hook!' );
 });
