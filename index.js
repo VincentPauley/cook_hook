590e185e1a79b09ff52995a2838d3ad8d8f8a034
Vincent Pauley <vinniepauley@gmail.com>
2017-08-21 21:24:16 -0500

    test write to db

--- index.js
+++ index.js
@@ -15,23 +15,41 @@ var promise = mongoose.connect( 'mongodb://localhost/myapp', {
 
 promise.then(function( db ) {
     // initial model
-    var recipeSchema = new Schema({
-        recipe_name : String,
-        tags : [String],
-        difficulty : Number,
-        ingredients : [{ name: String, qnty: String }],
-        cook_time : Number,
-        instructions : String
+    var userSchema = new Schema({
+        name: String,
+        username: { type: String, required: true, unique: true},
+        password: { type: String, required: true },
+        admin: Boolean,
+        location: String,
+        meta: {
+          age: Number,
+          website: String
+        },
+        created_at: Date,
+        updated_at: Date
     });
 
-    var Recipe = mongoose.model( 'Recipe', recipeSchema );
+    var User = mongoose.model( 'User', userSchema );
 
-    // attempt at one read
+    var newUser = User({
+        name: 'Vincent Pauley',
+        username: 'majorNoodle',
+        password: 'pass',
+        admin: true
+    });
+
+    newUser.save(function(err) {
+        if(err) throw err;
+
+        console.log( 'User Created' );
+    });
+
+    /* attempt at one read
     Recipe.find({}, function( err, recipe ) {
         if( err ) throw err;
 
         console.log( recipe );
-    });
+    });*/
 });
 
 
