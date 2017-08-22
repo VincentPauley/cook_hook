7c3b2b01c9ad990df60aea28004e376237417aa8
Vincent Pauley <vinniepauley@gmail.com>
2017-08-21 20:43:30 -0500

    document read test

--- index.js
+++ index.js
@@ -3,41 +3,38 @@ const express = require( 'express' ),
       mongoose = require( 'mongoose' ),
       Schema = mongoose.Schema;
 
+// set mongoose to use bluebird lib for promises rather than default
 mongoose.Promise = require( 'bluebird' );
 
 // provide the public Dir
 app.use( express.static( 'public' ) );
-
-
 
 var promise = mongoose.connect( 'mongodb://localhost/myapp', {
     useMongoClient: true
 });
 
 promise.then(function( db ) {
-    console.log( 'Connected the correct way' );
+    // initial model
+    var recipeSchema = new Schema({
+        recipe_name : String,
+        tags : [{ type: String }],
+        difficulty : Number,
+        ingredients : [{ type: String }],
+        cook_time : Number,
+        instructions : String
+    });
+
+    var Recipe = mongoose.model( 'Recipe', recipeSchema );
+
+    // attempt at one read
+    Recipe.find({}, function( err, recipe ) {
+        if( err ) throw err;
+
+        console.log( recipe );
+    });
 });
 
-/*
-// initial model
-var recipeSchema = new Schema({
-    recipe_name : String,
-    tags : [{ type: String }],
-    difficulty : Number,
-    ingredients : [{ type: String }],
-    cook_time : Number,
-    instructions : String
-});
 
-var Recipe = mongoose.model( 'Recipe', recipeSchema );
-
-// attempt at one read
-Recipe.find({}, function( err, recipe ) {
-    if( err ) throw err;
-
-    console.log( recipe );
-});
-*/
 
 app.get('/', function( req, res ) {
     res.send( 'Welcome to Cook-Hook!' );
