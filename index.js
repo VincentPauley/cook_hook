7db0338155a910df2a66f7129498f089c516cfcc
Vincent Pauley <vinniepauley@gmail.com>
2017-08-21 20:01:35 -0500

    mongo connection test

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const express = require( 'express' ),
 // provide the public Dir
 app.use( express.static( 'public' ) );
 
-const mongoDB = 'mongodb://127.0.0.1/recipes';
+mongoose.connect( 'mongodb://127.0.0.1/recipes' );
 
-mongoose.connect( mongoDB );
+var db = mongoose.connection;
 
-// default connection
-const db = mongoose.connection;
+db.on('error', console.error.bind( console, 'connection error:' ))
+db.once('open', function() {
+    console.log( 'we are connected!' );
+});
 
+/*
 // initial model
-const recipeSchema = new Schema({
+var recipeSchema = new Schema({
     recipe_name : String,
     tags : [{ type: String }],
     difficulty : Number,
@@ -23,7 +26,7 @@ const recipeSchema = new Schema({
     instructions : String
 });
 
-const Recipe = mongoose.model( 'Recipe', recipeSchema );
+var Recipe = mongoose.model( 'Recipe', recipeSchema );
 
 // attempt at one read
 Recipe.find({}, function( err, recipe ) {
@@ -31,7 +34,7 @@ Recipe.find({}, function( err, recipe ) {
 
     console.log( recipe );
 });
-
+*/
 
 app.get('/', function( req, res ) {
     res.send( 'Welcome to Cook-Hook!' );
